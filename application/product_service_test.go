f8e7bb05a349d1d66d185bdf115f88df0a68a7a6
nsantos06 <nicollasmk9@gmail.com>
2024-12-29 21:22:08 -0300

    gomock e Mockgen - 08

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,2 +1,28 @@
 package application_test
 
+import (
+	"testing"
+
+	"github.com/codeedu/go-hexagonal/application"
+	mock_application "github.com/codeedu/go-hexagonal/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProductService_Get(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistance := mock_application.NewMockProductPersistanceInterface(ctrl)
+	persistance.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistance: persistance,
+	}
+
+	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
--- go.mod
+++ go.mod
@@ -1,13 +1,25 @@
 module github.com/codeedu/go-hexagonal
 
-go 1.17
+go 1.22
+
+toolchain go1.23.4
 
 require github.com/stretchr/testify v1.10.0
 
 require (
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+)
+
+require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/mock v1.6.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/satori/go.uuid v1.2.0
+	google.golang.org/grpc v1.69.2
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
