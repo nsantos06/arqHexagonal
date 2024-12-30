84799c1a4e9545934aa0a34f65d7311dd0bf8dfe
nsantos06 <nicollasmk9@gmail.com>
2024-12-29 21:43:30 -0300

    Implementação do service - 09

--- application/product_service.go
+++ application/product_service.go
@@ -4,11 +4,56 @@ type ProductService struct {
 	Persistance ProductPersistanceInterface
 }
 
-func (s *ProductService) Get(id string) (ProductInterface, error){
+func (s *ProductService) Get(id string) (ProductInterface, error) {
 	product, err := s.Persistance.Get(id)
 	if err != nil {
 		return nil, err
 	}
 	return product, nil
+}
+
+func (s *ProductService) Create(nome string, price float64) (ProductInterface, error) {
+	product := NewProduct()
+	product.Name = nome
+	product.Price = price
+	_, err := product.IsValid()
+	if err != nil {
+		return &Product{}, err
+	}
+	result, err := s.Persistance.Save(product)
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
+}
+
+func (s *ProductService) Enable(product ProductInterface) (ProductInterface, error){
+	err := product.Enable()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Persistance.Save(product)
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
+}
+
+
+
+func (s *ProductService) Disable(product ProductInterface) (ProductInterface, error){
+	err := product.Disable()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Persistance.Save(product)
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
 }
 
