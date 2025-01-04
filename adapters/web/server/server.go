98b3307db3e64a6322bca915225ce6d0303c1f7b
nsantos06 <nicollasmk9@gmail.com>
2025-01-03 23:08:41 -0300

    Executando webserver

--- adapters/web/server/server.go
+++ adapters/web/server/server.go
@@ -12,34 +12,30 @@ import (
 	"github.com/gorilla/mux"
 )
 
-
-type Webserver struct{
+type Webserver struct {
 	Service application.ProductServiceInterface
 }
 
-func MakeNewWebServer()*Webserver{
+func MakeNewWebServer() *Webserver {
 	return &Webserver{}
 }
 
-
-func (w Webserver) Serve(){
+func (w Webserver) Serve() {
 	r := mux.NewRouter()
-	
+
 	n := negroni.New(
 		negroni.NewLogger(),
-
 	)
 
 	handler.MakeProductHandlers(r, n, w.Service)
 	http.Handle("/", r)
-	
-	
+
 	server := &http.Server{
 		ReadHeaderTimeout: 10 * time.Second,
-		WriteTimeout: 10 & time.Second,
-		Addr: ":8080",
-		Handler: http.DefaultServeMux,
-		ErrorLog: log.New(os.Stderr, "log:", log.Lshortfile),
+		WriteTimeout:      10 & time.Second,
+		Addr:              ":9000",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log:", log.Lshortfile),
 	}
 	err := server.ListenAndServe()
 	if err != nil {
